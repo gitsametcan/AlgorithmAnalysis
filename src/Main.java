a42c6f6230a6c8a53cf945c6df0297dabb1ab841
Samet CAN <108118230+gitsametcan@users.noreply.github.com>
2023-05-10 18:43:07 +0300

    inputs prepared

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		
 		//hp.count();
 		//System.out.println(hp.getCompNumber());
-		ReadHTML html = new ReadHTML("input");
+		ReadHTML html = new ReadHTML("typeoneinput");
 		
 		html.writeHTML();
 		
