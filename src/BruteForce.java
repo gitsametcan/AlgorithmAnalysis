dc75cd07bcfe99a315eee3d83b8ec89c40165da4
Hamza Ali İslamoğlu <hamza613468@gmail.com>
2023-05-17 22:57:15 +0300

    Comment Brute Force

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -16,20 +16,20 @@ public class BruteForce {
 		this.count();
 	}
 	
-	
+	//Compare characters one by one creating variables for the indexes and dividing them into char along with substrings.
 	private void count() {
-		int comprasion = 0;
-		List<Integer> temp = new ArrayList();
-		long startTime = System.currentTimeMillis();
+		int comprasion = 0; // Variable to keep the number of comparisons
+		List<Integer> temp = new ArrayList(); // List to store the indexes where the pattern matches
+		long startTime = System.currentTimeMillis(); // Start time of the function execution
 		int i = 0;
 		while(i<=this.htmlFile.length() - this.pattern.length()) {	
 			int j = this.pattern.length()-1;
 			int k = i+ this.pattern.length()-1;
 			while (this.htmlFile.substring(k, k+1).equals(this.pattern.substring(j,j+1)) && j!=0) {
-				
+				// Compare characters of the text and pattern from the end
 				comprasion ++;
 				if(j==1 && this.htmlFile.substring(k-1, k).equals(this.pattern.substring(j-1,j))) {
-					temp.add(i);
+					temp.add(i); // If the first two characters match, add the index to the list
 				}
 				j--;
 				k--;
